865cc5445b726ab65f13eea26fcfeddf995ff804
Ione Dias <lobodias.ione@gmail.com>
2022-03-21 19:19:51 -0300

    lista_framework

--- src/test/java/plataformaFilmes/PlataformaFilmesTest.java
+++ src/test/java/plataformaFilmes/PlataformaFilmesTest.java
@@ -5,12 +5,15 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import utils.RestUtils;
 
 import java.security.PublicKey;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static utils.RestUtils.post;
 
 public class PlataformaFilmesTest {
 
@@ -18,17 +21,17 @@ public class PlataformaFilmesTest {
 
     @Test
     public void validarLogin(){
-        RestAssured.baseURI = "http://localhost:8080/";
+        RestUtils.setBaseURI("http://localhost:8080/");
 
         String json = "{" +
                 "    \"email\": \"aluno@email.com\"," +
                 "    \"senha\": \"123456\"" +
                 "}";
 
-        Response response = post(json, ContentType.JSON, "auth");
+        RestUtils.post(json, ContentType.JSON, "auth");
 
-        assertEquals(200, response.statusCode());
-        String token = response.body().jsonPath().get("token");
+        assertEquals(200, RestUtils.getResponse().statusCode());
+        String token = RestUtils.getResponse().jsonPath().get("token");
 
         //comando para imprimir na console/tela
         //System.out.println(token);
@@ -36,12 +39,12 @@ public class PlataformaFilmesTest {
 
     @BeforeAll
     public static void validarLoginMap(){
-        RestAssured.baseURI = "http://localhost:8080/";
+        RestUtils.setBaseURI("http://localhost:8080/");
         Map<String, String> map = new HashMap<>();
         map.put("email", "aluno@email.com");
         map.put("senha", "123456");
 
-        Response response = post(map, ContentType.JSON, "auth");
+        Response response = RestUtils.post(map, ContentType.JSON, "auth");
 
         assertEquals(200, response.statusCode());
         token = response.body().jsonPath().get("token");
@@ -51,32 +54,13 @@ public class PlataformaFilmesTest {
     public void validarConsultaCategorias(){
         Map<String, String> header = new HashMap<>();
         header.put("Authorization", "Bearer " +token);
-
-        Response response = get(header, "categorias");
-
+        Response response = RestUtils.get(header, "categorias");
         assertEquals(200, response.statusCode());
+        //System.out.println(response.jsonPath().get().toString());
 
-        System.out.println(response.jsonPath().get().toString());
-
-    }
-
-    private Response get(Map<String, String> header, String endpoint) {
-        return RestAssured.given()
-                .relaxedHTTPSValidation()
-                .headers(header)
-                .when()
-                .get(endpoint)
-                .thenReturn();
-    }
-
-    public static Response post(Object json, ContentType contentType, String endpoint){
-        return RestAssured.given()
-                .relaxedHTTPSValidation()
-                .contentType(contentType)
-                .body(json)
-                .when()
-                .post(endpoint)
-                .thenReturn();
+        assertEquals("Terror", response.jsonPath().get("tipo[2]"));
+        List<String> listTipo = response.jsonPath().get("tipo");
+        assertTrue(listTipo.contains("Terror"));
 
     }
 
